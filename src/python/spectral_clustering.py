364975ba303d165578f4295ff548f54406173a4b
SHASHANK JAIN <neocfc@SHASHANKs-MacBook-Pro.local>
2015-12-15 00:44:50 -0500

    run all night n spectral

--- src/python/spectral_clustering.py
+++ src/python/spectral_clustering.py
@@ -1,18 +1,21 @@
 import networkx as nx
 import os
-from sklearn.cluster import KMeans
+from sklearn.cluster import SpectralClustering
 import Constants
 
-def kmeans_cluster(G, graph_name, num_clusters):
+def spectral_clustering(G, graph_name):
     #Find a way to figure out clusters number automatically
-    write_directory = os.path.join(Constants.KMEANS_PATH,graph_name)
+    num_clusters =12
+    write_directory = os.path.join(Constants.SPECTRAL_PATH,graph_name)
     if not os.path.exists(write_directory):
         os.makedirs(write_directory)
     nodeList = G.nodes()
     matrix_data = nx.to_numpy_matrix(G, nodelist = nodeList)
-    kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10)   
-    kmeans.fit(matrix_data)
-    label = kmeans.labels_
+    spectral = SpectralClustering(n_clusters=2,
+                                          eigen_solver='arpack',
+                                          affinity="nearest_neighbors")   
+    spectral.fit(matrix_data)
+    label = spectral.labels_
     clusters = {}
     
     for nodeIndex, nodeLabel in enumerate(label):
